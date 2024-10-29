28e37ebe3b5be96404e8609075d950ea14fc8a5d
Anton <antonh1803@gmail.com>
2024-10-29 22:10:43 +0200

    Massage about user

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,13 +1,13 @@
 from fastapi import Depends, HTTPException , APIRouter, Form, status
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import HTMLResponse, RedirectResponse, Response
+from fastapi.templating import Jinja2Templates
 from app.schemas import BookCreate ,Book, UserCreate, User, UserGet
-from starlette.requests import Request
-from sqlalchemy.orm import Session
 from app.db.crud import create_book_db , get_book_db , get_books_db, create_user_db, delete_book_db, edit_book_db
 from app.db.database import SessionLocal
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse, RedirectResponse, Response
+from starlette.requests import Request
+from sqlalchemy.orm import Session
 from typing import Annotated
-from fastapi.security import OAuth2PasswordRequestForm
 
 templates = Jinja2Templates(directory="app/templates")
 
@@ -44,28 +44,29 @@ async def delete_all_book(request: Request, skip: int = 0, limit: int = 100, db:
 
 @router.post("/books/", response_model=Book)
 # def create_book(book: BookCreate, db: Session = Depends(get_db)):
-def create_book(name: str =Form(...), author: str = Form(...), count_page: int = Form(...), db: Session = Depends(get_db)):
+async def create_book(name: str =Form(...), author: str = Form(...), count_page: int = Form(...), db: Session = Depends(get_db)):
     book = BookCreate(name=name, author=author, count_page=count_page)
     create_book_db(db=db, book=book)
     return RedirectResponse(url="/books/", status_code=303)
 
 @router.post("/usres/", response_model=UserGet)
 # def create_users(user: User, db: Session = Depends(get_db)):
-def create_users(username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+async def create_users(username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
     user = UserCreate(username=username, password=password)
-    return create_user_db(db=db, user=user)
+    # return create_user_db(db=db, user=user)
+    return RedirectResponse(url=f"/books/?message=Ви%20увійшли%20як%20{username}", status_code=303)
 
 @router.get("/books/", response_model=list[Book], response_class=HTMLResponse)
-def read_books(request: Request, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_books(request: Request, skip: int = 0, limit: int = 100, message: str = None, db: Session = Depends(get_db)):
     books = get_books_db(db, skip=skip, limit=limit)
     # return books
     if books:
-        return templates.TemplateResponse("books.html", {"request": request, "books": books})
+        return templates.TemplateResponse("books.html", {"request": request, "books": books, "message": message})
     else:
-        return templates.TemplateResponse("books.html", {"request": request, "books": None})
+        return templates.TemplateResponse("books.html", {"request": request, "books": None, "message": message})
 
 @router.get("/books/{book_id}", response_model=Book, response_class=HTMLResponse)
-def read_book(request: Request, book_id: int, db: Session = Depends(get_db)):
+async def read_book(request: Request, book_id: int, db: Session = Depends(get_db)):
     db_book = get_book_db(db, book_id=book_id)
     if db_book is None:
         raise HTTPException(status_code=404, detail="Book not found")
@@ -76,6 +77,22 @@ def read_book(request: Request, book_id: int, db: Session = Depends(get_db)):
 async def new_user_form(request: Request):
     return templates.TemplateResponse("create_user.html", {"request": request})
 
-@router.put("/book/{book_id}", response_model=Book)
-def edit_book(book: BookCreate, book_id: int, db: Session = Depends(get_db)):
-    return edit_book(db=db, book_id=book_id, book_data=book)
+@router.post("/book/{book_id}", response_model=Book)
+async def edit_book(
+    book_id: int,
+    name: str = Form(...),
+    author: str = Form(...),
+    count_page: int = Form(...),
+    db: Session = Depends(get_db)
+):
+    book_data = BookCreate(name=name, author=author, count_page=count_page)
+    updated_book = edit_book_db(db=db, book_data=book_data, book_id=book_id)
+    return RedirectResponse(url="/books/", status_code=303)
+
+@router.get("/book/edit/all/{book_id}", response_class=HTMLResponse)
+async def edit_all_book(request: Request, book_id: int, db: Session = Depends(get_db)):
+    db_book = get_book_db(db, book_id=book_id)
+    if db_book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+    
+    return templates.TemplateResponse("edit_book.html", {"request": request, "book": db_book})
